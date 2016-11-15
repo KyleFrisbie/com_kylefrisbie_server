23502a7db6f005771eb948f601f928af8efebea0
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-11-14 21:44:50 -0700

    added test config and initial source

---
+++ source/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kylel on 11/14/2016.
+ */
--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -1,9 +1,17 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var server = require('../source/index');
 var should = chai.should();
 
 describe('blog-posts', function () {
-    it('should get a list of all blog posts on /posts GET');
+    it('should get a list of all blog posts on /posts GET', function (done) {
+        chai.request(server)
+            .get('/posts')
+            .end(function (err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
     it('should get a single blog post on /posts/\<id\> GET');
     it('should post a single blog post on /posts :wqaPOST');
     it('should update a single blog post on /posts/\<id\> PUT');
