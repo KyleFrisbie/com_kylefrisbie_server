07209055adacea2c82a689f2042ba48dfd60c65c
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-11-14 21:59:42 -0700

    created initial connection to mongodb

--- source/index.js
+++ source/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by kylel on 11/14/2016.
  */
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var config = require('config');
+
+var app = express();
+var port = 3000;
+var dbConfig = config.get('server.dbConfig');
+
+mongoose.connect('mongodb://' + dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.name);
