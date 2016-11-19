0a5865130cd7d54e4a1d9b667df3f5d419f8883a
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-11-19 16:05:52 -0700

    created initial user test scaffold

--- test/test-users.js
+++ test/test-users.js
@@ -10,5 +10,10 @@ chai.use(chaiHttp);
 chai.use(chaiAsPromised);
 
 describe('users', function () {
-    it('should create a user');
+    it('should create generate dummy users to test');
+    it('should get a list of all users on /users GET');
+    it('should get a single user on /users/\<id\> GET');
+    it('should post a single user on /users/create POST');
+    it('should update a single user on /users/\<id\> PUT');
+    it('should delete a single user on /users/\<id\> DELETE');
 });
